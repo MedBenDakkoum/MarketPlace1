78e66d0068a6f412b609f527a3ce275b15a5e399
Mohamed Ben Dakkoum <bendekoummed@gmail.com>
2024-01-29 17:13:52 +0100

    MP Dashboard

--- client/src/App.js
+++ client/src/App.js
@@ -1,34 +1,35 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 // import './App.css';
 import {
   BrowserRouter,
   Routes,
   Route,
   Link,
-  useNavigate
+  useNavigate,
 } from "react-router-dom";
-import Categories from './Pages/Categories';
-import Home from './Pages/Home';
-import Error404 from './Pages/Error404';
-import RootLayout from './Pages/RootLayout';
-import { ContextStore } from './ContextStore';
-import AdminRootLayout from './Pages/AdminRootLayout';
-import AdminLogOut from './Pages/AdminLogOut';
-import Login from './Pages/Login';
-import Register from './Pages/Register';
-import LogOut from './Pages/LogOut';
-import Store from './Pages/Store';
-import AdminHome from './components/Dashboard/AdminHome';
-import MpProducts from './components/Dashboard/MpProducts';
-import MpProductEdit from './components/Dashboard/MpProductEdit';
-import MpOrders from './components/Dashboard/MpOrders';
-import MpTransactions from './components/Dashboard/MpTransactions';
-import MpSellers from './components/Dashboard/MpSellers';
-import MpSellerEdit from './components/Dashboard/MpSellerEdit';
-import {MpConfig} from './components/Dashboard/MpConfig';
-import MpProductComboEdit from './components/Dashboard/MpProductCombo/MpProductComboEdit'
-import MpOrdersView from './components/Dashboard/MpOrders/MpOrdersView';
-import MpSingleOrderView from './components/Dashboard/MpOrders/MpSingleOrderView'
+import Mp from "./components/Dashboard/Mp";
+import Categories from "./Pages/Categories";
+import Home from "./Pages/Home";
+import Error404 from "./Pages/Error404";
+import RootLayout from "./Pages/RootLayout";
+import { ContextStore } from "./ContextStore";
+import AdminRootLayout from "./Pages/AdminRootLayout";
+import AdminLogOut from "./Pages/AdminLogOut";
+import Login from "./Pages/Login";
+import Register from "./Pages/Register";
+import LogOut from "./Pages/LogOut";
+import Store from "./Pages/Store";
+import AdminHome from "./components/Dashboard/AdminHome";
+import MpProducts from "./components/Dashboard/MpProducts";
+import MpProductEdit from "./components/Dashboard/MpProductEdit";
+import MpOrders from "./components/Dashboard/MpOrders";
+import MpTransactions from "./components/Dashboard/MpTransactions";
+import MpSellers from "./components/Dashboard/MpSellers";
+import MpSellerEdit from "./components/Dashboard/MpSellerEdit";
+import { MpConfig } from "./components/Dashboard/MpConfig";
+import MpProductComboEdit from "./components/Dashboard/MpProductCombo/MpProductComboEdit";
+import MpOrdersView from "./components/Dashboard/MpOrders/MpOrdersView";
+import MpSingleOrderView from "./components/Dashboard/MpOrders/MpSingleOrderView";
 //import {Action as logoutAction} from './pages/Logout.js'
 
 // const router = createBrowserRouter(router)
@@ -36,33 +37,33 @@ import MpSingleOrderView from './components/Dashboard/MpOrders/MpSingleOrderView
 export default function App() {
   const navigate = useNavigate();
   return (
-      <Routes>
-        <Route path="/" context={ContextStore} element={<RootLayout />}>
-            <Route index element={<Home />} />
-            <Route path="s" element={<Store/>} />
-        </Route>
-        <Route path="/auth" element={<RootLayout />}>
-          <Route index element={<Login />} />
-          <Route path="login" element={<Login navigate={navigate}/>} />
-          <Route path="register" element={<Register navigate={navigate}/>} />
-          <Route path="logout" exact element={<LogOut navigate={navigate}/>}  />
-        </Route>
-        <Route path="/admin" element={<AdminRootLayout />}>
-          <Route index element={<AdminHome />} />
-          <Route path="mp/config" element={<MpConfig />} />
-          <Route path="mp/sellers" element={<MpSellers />} />
-          <Route path="mp/sellers/:id" element={<MpSellerEdit />} />
-          <Route path="mp/products" element={<MpProducts />} />
-          <Route path="mp/products/:id" element={<MpProductEdit />} />
-          <Route path="mp/productcombo/:id" element={<MpProductComboEdit/>}/>
-          <Route path="mp/sellers/:id/orders" element={<MpOrdersView/>}/>
-          <Route path="mp/orders/:id" element={<MpSingleOrderView/>}/>
-          <Route path="mp/orders" element={<MpOrders />} />
-          <Route path="register" element={<Register />} />
-          <Route path="logout" exact element={LogOut} />
-        </Route>
-        <Route path="*" element={<Error404 />} />
-      </Routes>
+    <Routes>
+      <Route path="/" context={ContextStore} element={<RootLayout />}>
+        <Route index element={<Home />} />
+        <Route path="s" element={<Store />} />
+      </Route>
+      <Route path="/auth" element={<RootLayout />}>
+        <Route index element={<Login />} />
+        <Route path="login" element={<Login navigate={navigate} />} />
+        <Route path="register" element={<Register navigate={navigate} />} />
+        <Route path="logout" exact element={<LogOut navigate={navigate} />} />
+      </Route>
+      <Route path="/admin" element={<AdminRootLayout />}>
+        <Route index element={<AdminHome />} />
+        <Route path="mp/config" element={<MpConfig />} />
+        <Route path="mp/sellers" element={<MpSellers />} />
+        <Route path="mp/sellers/:id" element={<MpSellerEdit />} />
+        <Route path="mp/products" element={<MpProducts />} />
+        <Route path="mp/products/:id" element={<MpProductEdit />} />
+        <Route path="mp/productcombo/:id" element={<MpProductComboEdit />} />
+        <Route path="mp/sellers/:id/orders" element={<MpOrdersView />} />
+        <Route path="mp/orders/:id" element={<MpSingleOrderView />} />
+        <Route path="mp/orders" element={<MpOrders />} />
+        <Route path="mp" element={<Mp />} />
+        <Route path="register" element={<Register />} />
+        <Route path="logout" exact element={LogOut} />
+      </Route>
+      <Route path="*" element={<Error404 />} />
+    </Routes>
   );
 }
-
--- client/src/components/Dashboard/AdminSidebar.jsx
+++ client/src/components/Dashboard/AdminSidebar.jsx
@@ -1,78 +1,127 @@
-import React from 'react'
-import { useRef } from 'react';
-import 
-{BsCart3, BsGrid1X2Fill, BsFillArchiveFill, BsFillGrid3X3GapFill, BsPeopleFill, 
-  BsListCheck, BsMenuButtonWideFill, BsFillGearFill}
- from 'react-icons/bs'
-import { useNavigate } from 'react-router-dom';
+import React from "react";
+
+import {
+  BsCart3,
+  BsGrid1X2Fill,
+  BsFillArchiveFill,
+  BsFillGrid3X3GapFill,
+  BsPeopleFill,
+  BsListCheck,
+  BsMenuButtonWideFill,
+  BsFillGearFill,
+} from "react-icons/bs";
+import { useNavigate } from "react-router-dom";
 let oc = 0;
-const ToggleMp = () => {
-    if(oc){
-        document.getElementsByClassName('marketplace-dropdown')[0].style = "height:0"
-        oc=0;    
-    }else{
-        document.getElementsByClassName('marketplace-dropdown')[0].style = "height:160px"
-        oc=1;
+
+function AdminSidebar({ openSidebarToggle, OpenSidebar }) {
+  const navigate = useNavigate();
+  const ToggleMp = () => {
+    if (oc) {
+      document.getElementsByClassName("marketplace-dropdown")[0].style =
+        "height:0";
+      oc = 0;
+    } else {
+      document.getElementsByClassName("marketplace-dropdown")[0].style =
+        "height:160px";
+      oc = 1;
     }
-}
-
-function AdminSidebar({openSidebarToggle, OpenSidebar}) {
-    const navigate= useNavigate();
+    navigate("/admin/mp");
+  };
   return (
-    <aside id="sidebar" className={openSidebarToggle ? "sidebar-responsive": ""}>
-        <div className='sidebar-title'>
-            <div className='sidebar-brand'>
-                <BsCart3  className='icon_header'/> SHOP
-            </div>
-            <span className='icon close_icon' onClick={OpenSidebar}>X</span>
+    <aside
+      id="sidebar"
+      className={openSidebarToggle ? "sidebar-responsive" : ""}
+    >
+      <div className="sidebar-title">
+        <div className="sidebar-brand sli">
+          <BsCart3 className="icon_header" /> SHOP
         </div>
+        <span className="icon close_icon" onClick={OpenSidebar}>
+          X
+        </span>
+      </div>
 
-        <ul className='sidebar-list'>
-            <li className='sidebar-list-item'>
-                <a href="/admin">
-                    <BsGrid1X2Fill className='icon'/> Dashboard
-                </a>
-            </li>
-            <li className='sidebar-list-item'>
-                <a href="">
-                    <BsFillArchiveFill className='icon'/> Products
-                </a>
-            </li>
-            <li className='sidebar-list-item'>
-                <a href="">
-                    <BsFillGrid3X3GapFill className='icon'/> Categories
-                </a>
-            </li>
-            <li className='sidebar-list-item'>
-                <a href="">
-                    <BsPeopleFill className='icon'/> Customers
-                </a>
-            </li>
-            <li className='sidebar-list-item'>
-                <a onClick={ToggleMp}>
-                    <BsListCheck className='icon'/> Marketplace
-                </a>
-            </li>
-            <div className='marketplace-dropdown'>
-                    <a onClick={(e)=>{navigate("/admin/mp/config")}}>Configuration</a>
-                    <a onClick={(e)=>{navigate("/admin/mp/sellers")}}>Sellers</a>
-                    <a onClick={(e)=>{navigate("/admin/mp/products")}}>Products</a>
-                    <a onClick={(e)=>{navigate("/admin/mp/orders")}}>Orders</a>
-                    <a onClick={(e)=>{navigate("/admin/mp/transactions")}}>Transactions</a>
-            </div>
-            <li className='sidebar-list-item'>
-                <a href="">
-                    <BsMenuButtonWideFill className='icon'/> Reports
-                </a>
-            </li>
-            <li className='sidebar-list-item'>
-                <a href="">
-                    <BsFillGearFill className='icon'/> Setting
-                </a>
-            </li>
-        </ul>
+      <ul className="sidebar-list">
+        <li className="sidebar-list-item">
+          <a href="/admin">
+            <BsGrid1X2Fill className="icon" /> Dashboard
+          </a>
+        </li>
+        <li className="sidebar-list-item">
+          <a href="">
+            <BsFillArchiveFill className="icon" /> Products
+          </a>
+        </li>
+        <li className="sidebar-list-item">
+          <a href="">
+            <BsFillGrid3X3GapFill className="icon" /> Categories
+          </a>
+        </li>
+        <li className="sidebar-list-item">
+          <a href="">
+            <BsPeopleFill className="icon" /> Customers
+          </a>
+        </li>
+        <li className="sidebar-list-item">
+          <a className="sli" onClick={ToggleMp}>
+            <BsListCheck className="icon" /> Marketplace
+          </a>
+        </li>
+        <div className="marketplace-dropdown">
+          <a
+            className="mpa"
+            onClick={(e) => {
+              navigate("/admin/mp/config");
+            }}
+          >
+            Configuration
+          </a>
+          <a
+            className="mpa"
+            onClick={(e) => {
+              navigate("/admin/mp/sellers");
+            }}
+          >
+            Sellers
+          </a>
+          <a
+            className="mpa"
+            onClick={(e) => {
+              navigate("/admin/mp/products");
+            }}
+          >
+            Products
+          </a>
+          <a
+            className="mpa"
+            onClick={(e) => {
+              navigate("/admin/mp/orders");
+            }}
+          >
+            Orders
+          </a>
+          <a
+            className="mpa"
+            onClick={(e) => {
+              navigate("/admin/mp/transactions");
+            }}
+          >
+            Transactions
+          </a>
+        </div>
+        <li className="sidebar-list-item">
+          <a href="">
+            <BsMenuButtonWideFill className="icon" /> Reports
+          </a>
+        </li>
+        <li className="sidebar-list-item">
+          <a href="">
+            <BsFillGearFill className="icon" /> Setting
+          </a>
+        </li>
+      </ul>
     </aside>
-  )
+  );
 }
 
-export default AdminSidebar
+export default AdminSidebar;
